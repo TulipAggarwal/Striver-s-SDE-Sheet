ea0dcd18bb10e74b2e0467beeb6dc0819929f2cf
Tulip Aggarwal <93984886+TulipAggarwal@users.noreply.github.com>
2023-01-02 19:21:54 +0530

    Delete Find the duplicate in an array of N+1 integers.py

--- Find the duplicate in an array of N+1 integers.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def findDuplicate(self, nums: List[int]) -> int:
-        sets = set()
-        for i in nums:
-            if i in sets:
-                return i
-            sets.add(i)
