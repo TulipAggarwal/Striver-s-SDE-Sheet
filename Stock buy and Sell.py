2fc94152dbd656a6a05a39a8c94c2d0ffe21fd1f
Tulip Aggarwal <93984886+TulipAggarwal@users.noreply.github.com>
2023-04-12 16:58:10 +0530

    Delete Stock buy and Sell.py

--- Stock buy and Sell.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        profit = 0
-        buy = prices[0]
-        for cur_price in prices:
-            buy = min(buy,cur_price)
-            profit = max(profit, (cur_price - buy))
-
-        return profit
