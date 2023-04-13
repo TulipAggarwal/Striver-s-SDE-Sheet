a13c6e09c4df370a36b6ca60c96ac2f77860bd0d
Tulip Aggarwal <93984886+TulipAggarwal@users.noreply.github.com>
2023-04-13 22:23:09 +0530

    Update Rotate Matrix.py

--- Day-2/Rotate Matrix.py
+++ Day-2/Rotate Matrix.py
@@ -1,3 +1,4 @@
+#First Approach 
 class Solution:
     def rotate(self, matrix: List[List[int]]) -> None:
         n = len(matrix[0])
@@ -8,3 +9,15 @@ class Solution:
                 matrix[n - 1 - i][n - j - 1] = matrix[j][n - 1 -i]
                 matrix[j][n - 1 - i] = matrix[i][j]
                 matrix[i][j] = tmp
+                
+#Second Approach
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        n = len(matrix)
+        for r in range(n):
+            for c in range(r):
+                #Transposing the matrix
+                matrix[r][c], matrix[c][r] = matrix[c][r],matrix[r][c]
+        #Reversing each row after the matrix is transposed to get the rotated image
+        for r in matrix:
+            r.reverse()
