35b7214b8b74b59f0eaec989936fd379658e701e
Tulip Aggarwal <93984886+TulipAggarwal@users.noreply.github.com>
2023-04-20 22:09:43 +0530

    Update Rotate Matrix.py

--- Day-2/Rotate Matrix.py
+++ Day-2/Rotate Matrix.py
@@ -9,7 +9,8 @@ class Solution:
                 matrix[n - 1 - i][n - j - 1] = matrix[j][n - 1 -i]
                 matrix[j][n - 1 - i] = matrix[i][j]
                 matrix[i][j] = tmp
-                
+   
+
 #Second Approach
 class Solution:
     def rotate(self, matrix: List[List[int]]) -> None:
